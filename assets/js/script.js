a833db851e01dde821795a707ea4dca459c2a625
GDim01 <georgedimitrov86@gmail.com>
2022-12-21 19:48:46 +0000

    Check wins via both diagonals

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,16 @@ function checkWinner () {
 
     //Check column win conditions
     for (let i = 0; i < 3; i++) {
-        if (boardArray[0][i] === boardArray[1][i] && boardArray[0][i] === boardArray[2][i] && boardArray[0][i] !== '') {
+        if (boardArray[0][i] === boardArray[1][i] && boardArray[0][i] === boardArray[2][i] && boardArray[0][i] !== "") {
+            return;
+        }
+    }
+
+    //Check diagonal win conditions
+    for (let i = 0; i < 3; i++) {
+        if (boardArray[0][0] === boardArray[1][1] && boardArray[0][0] === boardArray[2][2] && boardArray[0][0] !== "") {
+            return;
+        } else if (boardArray[0][2] === boardArray[1][1] && boardArray[0][2] === boardArray[2][0] && boardArray[0][2] !== "") {
             return;
         }
     }
