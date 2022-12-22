932cdf0ba85bbc46a430cd7cf1de3cebdeed3582
GDim01 <georgedimitrov86@gmail.com>
2022-12-22 01:18:28 +0000

    Adjust scores correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 const playerOne = "O"
 const playerTwo = "X"
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            this.getAttribute("restart-game");
+            reset();
+        });
+    }
+
+});
+
 let boardArray = [
     ["", "", ""],
     ["", "", ""],
@@ -35,6 +47,10 @@ function selectCell (cell, index) {
             boardArray[Math.floor(index / 3)][index % 3] = playerTwo;
         }
     }
+
+    cell.removeEventListener("click", selectCell);
+
+    checkWinner();
 }
 
 /**
@@ -44,6 +60,7 @@ function checkWinner () {
     //Check row win conditions
     for (let i = 0; i < 3; i++) {
         if (boardArray[i][0] === boardArray[i][1] && boardArray[i][0] === boardArray[i][2] && boardArray[i][0] !== "") {
+            showResult(boardArray[i][0]);
             return;
         }
         
@@ -52,6 +69,7 @@ function checkWinner () {
     //Check column win conditions
     for (let i = 0; i < 3; i++) {
         if (boardArray[0][i] === boardArray[1][i] && boardArray[0][i] === boardArray[2][i] && boardArray[0][i] !== "") {
+            showResult(boardArray[0][i]);
             return;
         }
     }
@@ -59,8 +77,10 @@ function checkWinner () {
     //Check diagonal win conditions
     for (let i = 0; i < 3; i++) {
         if (boardArray[0][0] === boardArray[1][1] && boardArray[0][0] === boardArray[2][2] && boardArray[0][0] !== "") {
+            showResult(boardArray[0][0]);
             return;
         } else if (boardArray[0][2] === boardArray[1][1] && boardArray[0][2] === boardArray[2][0] && boardArray[0][2] !== "") {
+            showResult(boardArray[0][2]);
             return;
         }
     }
@@ -76,6 +96,7 @@ function checkWinner () {
     }
 
     if (filledCells === 9) {
+        showResult("Tie");
         return;
     }
 
@@ -83,30 +104,55 @@ function checkWinner () {
 
 function showResult (playerSymbol) {
     let result = document.querySelector(".result");
-    let p1Streak = parseInt(document.getElementById("p1-streak").value);
-    let p1Score = parseInt(document.getElementById("p1-score").value);
-    let p2Score = parseInt(document.getElementById("p2-score").value);
-    let p2Streak = parseInt(document.getElementById("p2-streak").value);
     if (playerSymbol === playerOne) {
-        result.innerHTML = 'Player 1 Wins!';
-        p1Score++;
-        p2Score = 0;
-        if (p1Score > p1Streak) {p1Streak++;}
-  } else if (playerSymbol === playerTwo) {
+        result.innerHTML = 'Player 1 Win!';
+        scoreAdjustment(1);
+    } else if (playerSymbol === playerTwo) {
         result.innerHTML = 'Player 2 Win!';
-        p2Score++;
-        p1Score = 0;
-        if (p2Score > p2Streak) {p2Streak++;}
+        scoreAdjustment(2);
   } else {
         result.innerHTML = 'Tie!';
   }
   result.style.display = 'flex';
 }
 
-function reset () {
-    if () {
-        
+function scoreAdjustment (winner) {
+    let p1Streak = parseInt(document.getElementById("p1-streak").innerText);
+    let p1Score = parseInt(document.getElementById("p1-score").innerText);
+    let p2Score = parseInt(document.getElementById("p2-score").innerText);
+    let p2Streak = parseInt(document.getElementById("p2-streak").innerText);
+
+    if (winner === 1) {
+        ++p1Score;
+        console.log(p1Score);
+        document.getElementById("p1-score").innerText = p1Score;
+        document.getElementById("p2-score").innerText = 0;
+        if (p1Score > p1Streak) {document.getElementById("p1-streak").innerText = ++p1Streak;}
+        reset();
     }
+
+    if (winner === 2) {
+        document.getElementById("p2-score").innerText = ++p2Score;
+        document.getElementById("p1-score").innerText = 0;
+        if (p2Score > p2Streak) {document.getElementById("p2-streak").innerText = ++p2Streak;}
+        reset();
+    }
+}
+
+function reset () {
+    let result = document.querySelector(".result");
+    let playerTurn = document.querySelector(".player-turn")
+    result.style.display = "none"
+    playerTurn.innerHTML = "Player 1"
+
+    boardArray = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]
+
+    gameStart();
+    
 }
 
 gameStart();
